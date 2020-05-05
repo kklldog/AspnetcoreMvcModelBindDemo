07c41dc84c1b50d96d78d633662e13a5c579cd28
kklldog <kklldog@qq.com>
2020-05-05 11:31:41 +0800

    添加项目文件。

---
+++ AspnetCoreModelbindDemo/Controllers/GetFromUrlController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspnetCoreModelbindDemo.Models;
+
+namespace AspnetCoreModelbindDemo.Controllers
+{
+    public class FromUrlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("FromUrl");
+        }
+
+        public IActionResult Test()
+        {
+            var name = Request.Query["name"];
+            return Content(name);
+        }
+
+        public IActionResult Test1(string name)
+        {
+            return Content(name);
+        }
+        public IActionResult Test2([FromQuery]string id)
+        {
+            return Content(id);
+        }
+
+        [Route("FromUrl/test3/{name}/{id}")]
+        public IActionResult Test3(string name, int id)
+        {
+            return Content($"{name}/{id}");
+        }
+
+        [HttpGet("FromUrl/test4/{name}/{id}")]
+        public IActionResult Test4(string name, int id)
+        {
+            return Content($"{name}/{id}");
+        }
+
+        [HttpGet("FromUrl/test5/{name}/{id}")]
+        public IActionResult Test5([FromRoute(Name ="name")]string xm, [FromRoute(Name = "id")]int bh)
+        {
+            return Content($"{xm}/{bh}");
+        }
+    }
+}
---
+++ AspnetCoreModelbindDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspnetCoreModelbindDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
